f7218513b9930900f45624a5a38e44c6c7cc297a
suminiee <chansu0114@naver.com>
2025-03-28 16:46:02 +0900

    Q2164

--- SUMIN/week1/SOO_Q1929.java
+++ SUMIN/week1/SOO_Q1929.java
@@ -1,12 +1,21 @@
 package SUMIN.week1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class SOO_Q1929 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int m = sc.nextInt();
-        int n = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+//        Scanner sc = new Scanner(System.in);
+//        int m = sc.nextInt();
+//        int n = sc.nextInt();
+        int m = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
 
         boolean[] isPrime = new boolean[n + 1];  // 기본값 false (소수 여부 저장)
         for (int i = 2; i <= n; i++) {
@@ -27,6 +36,6 @@ public class SOO_Q1929 {
             }
         }
 
-        sc.close();
+//        sc.close();
     }
 }
