2fd6c1f6ae0a8ac0d17c3faff4b37ae9e248142f
yeomsky95 <yeomsky95@gmail.com>
2025-04-01 11:05:58 +0900

    2주차 과제

---
+++ DABIN/WEEK1/W10974.java
@@ -0,0 +1,41 @@
+package DABIN.WEEK1;
+
+import java.util.Scanner;
+
+public class W10974 {
+     void permutation(int[] arr, int depth, int N, int M, boolean[] visited, int[] out) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                System.out.print(out[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (visited[i] != true) {
+                visited[i] = true;
+                out[depth] = arr[i];
+                permutation(arr, depth + 1, N, M, visited, out);
+                visited[i] = false;
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        int[] numArr = new int[N];
+        boolean[] visited = new boolean[N];
+        int[] out = new int[N];
+        for (int i = 0; i < N; i++) {
+            numArr[i] = i + 1;
+        }
+        W10974 m = new W10974();
+        m.permutation(numArr, 0, N, N, visited, out);
+
+        sc.close();
+    }
+}
