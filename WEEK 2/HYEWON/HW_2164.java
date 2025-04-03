c2d1b75f0d571426ae1db1ad89644974ec49e21c
Hyewon Lee <hooho931@gmail.com>
2025-04-03 18:00:17 +0900

    edit: 클래스명 수정

--- WEEK 2/HYEWON/HW_2164.java
+++ WEEK 2/HYEWON/HW_2164.java
@@ -8,7 +8,7 @@ import java.util.Queue;
 1번 카드가 제일 위에, N번 카드가 제일 아래인 상태 순서로 카드가 있다
 제일 위에 있는 카드 -> 버림 / 그 다음 제일 위에 있는 카드를 제일 아래에 있는 카드 밑으로 옮긴다
  */
-class Main{
+class HW_2164{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
