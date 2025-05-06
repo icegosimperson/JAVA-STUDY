8631befea5251761ee26b4c8752a7d8f4cc00afb
suminiee <chansu0114@naver.com>
2025-05-07 00:33:21 +0900

    8911 12726 solve

---
+++ SUMIN/week6/Q17276.java
@@ -0,0 +1,10 @@
+package SUMIN.week6;
+
+import java.util.*;
+import java.io.*;
+
+public class Q17276 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    }
+}
