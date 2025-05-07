f40bcf33f2a973fc942e3a447877718c45ba5a38
westjin <uko802@naver.com>
2025-05-08 00:03:59 +0900

    BOJ solve

---
+++ SEOJIN/WEEK5/SJ_11729.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class SJ_11729 {
+
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        hanoi(N,1,3,2);
+        System.out.println(sb);
+
+    }
+    private static void hanoi(int n, int from, int to, int aux){
+        //N 원판 수
+        // from 현재 기둥
+        // to 목표
+        // aux 보조
+        if(n==1){
+            sb.append(from + " "+to+"\n");
+            return;
+        }
+
+        hanoi(n-1,from, aux, to);
+        sb.append(from + " "+to+"\n");
+        hanoi(n-1,aux,to,from);
+    }
+
+
+    }
+
